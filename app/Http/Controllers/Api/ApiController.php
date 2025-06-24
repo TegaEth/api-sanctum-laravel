802e7dc3fce688ee87b52e4edb9b4bedefaa6fc7
bunchiochi <ochiojie@gmail.com>
2025-06-24 23:27:35 +0300

    additions

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+
+class ApiController extends Controller
+{
+    //
+}
